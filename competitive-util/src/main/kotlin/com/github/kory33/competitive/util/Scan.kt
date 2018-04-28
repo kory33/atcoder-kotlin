880c1c4b83c061f1a3129cea1c9298775282e592
kory33 <6561358+kory33@users.noreply.github.com>
2018-04-28 23:06:31 +0800

    fix slow implementation of scanIndexed

--- competitive-util/src/main/kotlin/com/github/kory33/competitive/util/Scan.kt
+++ competitive-util/src/main/kotlin/com/github/kory33/competitive/util/Scan.kt
@@ -1,23 +1,33 @@
 package com.github.kory33.competitive.util
 
+import java.util.*
+
 // region Scanning
 
-private inline fun <T, R> Iterable<T>.scanIndexed(initial: R, operation: (Int, R, T) -> R): List<R>
-        = foldIndexed(listOf()) { index, list, elem -> list + operation(index, list.lastOrNull() ?: initial, elem) }
+private inline fun <T, R> Collection<T>.scanIndexed(initial: R, operation: (Int, R, T) -> R): List<R> {
+    val result = ArrayList<R>(size)
+    var prevResult = initial
+    forEachIndexed { index, element ->
+        val elementResult = operation(index, prevResult, element)
+        result.add(elementResult)
+        prevResult = elementResult
+    }
+    return result
+}
 
-private inline fun <T> Iterable<T>.scanIndexed1(operation: (Int, T, T) -> T): List<T>
+private inline fun <T> Collection<T>.scanIndexed1(operation: (Int, T, T) -> T): List<T>
         = drop(1).scanIndexed(first(), operation)
 
-private inline fun <T, R> Iterable<T>.scan(initial: R, operation: (R, T) -> R): List<R>
+private inline fun <T, R> Collection<T>.scan(initial: R, operation: (R, T) -> R): List<R>
         = scanIndexed(initial) {i, list, elem -> operation(list, elem) }
 
-private inline fun <T> Iterable<T>.scan1(operation: (T, T) -> T): List<T>
+private inline fun <T> Collection<T>.scan1(operation: (T, T) -> T): List<T>
         = scanIndexed1 {i, list, elem -> operation(list, elem)}
 
 // endregion
 
 // region Util implementations
 
-private fun Iterable<Int>.accumulate() = this.scan(0) { a, b -> a + b }
+private fun Collection<Int>.accumulate() = this.scan(0) { a, b -> a + b }
 
 // endregion
